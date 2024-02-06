1a8b1348cb07be9b54a5d76d5ecba33ba5a1be90
Vivid-Vortex <ctntc9.dpk@gmail.com>
2024-02-07 03:04:25 +0530

    Comments formatted for Order --> cacade

--- NonReactiveJpa/src/main/java/com/demo/jpa/nonreactive/entity/Order.java
+++ NonReactiveJpa/src/main/java/com/demo/jpa/nonreactive/entity/Order.java
@@ -40,9 +40,10 @@ public class Order {
     //mappedBy = "order", order is the field name of Order object inside OrderItem
     @OneToMany(
             mappedBy = "order", // This is the name of the Order type variable inside orderItem entity, since this is a bidirectional relationship
-            cascade = CascadeType.ALL,
+
             // cascade = CascadeType.ALL: If we perform any ooperation on order entity, that operation will also be cascaded to order item entity
             //such as PERSIST, MERGE, REMOVE, DETACH, REFRESH, ALL (This includes all of the above cascade options.)
+            cascade = CascadeType.ALL,
             orphanRemoval = true // If order is deleted, all orderItem wil also be removed
     )
     private List<OrderItem> orderItems = new ArrayList<>();
